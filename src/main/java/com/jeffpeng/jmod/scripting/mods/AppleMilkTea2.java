26c4f38b97cbdcfbad34c5847202a89b524142b5
Nicholas Marshall <nick.marshall@hookmobile.com>
2016-12-21 19:01:20 -0500

    bumped version, cleanup AMT2

--- src/main/java/com/jeffpeng/jmod/scripting/mods/AppleMilkTea2.java
+++ src/main/java/com/jeffpeng/jmod/scripting/mods/AppleMilkTea2.java
@@ -16,26 +16,30 @@ public class AppleMilkTea2 extends OwnedObject {
 	}
 
 	public void addClayPanRecipe(String out, String input, String texture, String display) {
-		if(owner.testForMod("DCsAppleMilk")) new AddClayPanRecipe(owner, out, input, texture, display);
+		if(owner.testForMod("DCsAppleMilk")) 
+			new AddClayPanRecipe(owner, out, input, texture, display);
 	}
 	
-	public void addClayPanRecipe(String out, String outJpBowl, String input, String texture, String display) {
-		if(owner.testForMod("DCsAppleMilk")) new AddClayPanRecipe(owner, outJpBowl, input, texture, display);
+	public void addIronPlateOvenRecipe(String output, String input, int cookingTime) {
+		boolean isOvenRecipe = true;
+		
+		if(owner.testForMod("DCsAppleMilk")) 
+			new AddIronPlateRecipe(owner, output, input, cookingTime, isOvenRecipe);
 	}
 	
-	public void addIronPlateRecipe(String output, String input, int cookingTime, boolean isOvenRecipe) {
-		if(owner.testForMod("DCsAppleMilk")) new AddIronPlateRecipe(owner, output, input, cookingTime, isOvenRecipe);
+	public void addIronPlateRecipe(String output, String input, int cookingTime) {
+		boolean isOvenRecipe = false;
+		
+		if(owner.testForMod("DCsAppleMilk")) 
+			new AddIronPlateRecipe(owner, output, input, cookingTime, isOvenRecipe);
 	}
 	
-	public void addProcessorRecipe(String output, String secondaryOutput, String inputs,
-			boolean isFoodRecipe, float secondaryChance, boolean forceReturnContainer, int tier) {
-		if(owner.testForMod("DCsAppleMilk")) new AddProcessorRecipe(owner, output, secondaryOutput, inputs, isFoodRecipe, secondaryChance, forceReturnContainer, tier);
-	}
-	
-	public void addProcessorRecipe(String output, String secondaryOutput, String inputs, float secondaryChance, int tier) {
+	public void addJawCrusherRecipe(String output, String secondaryOutput, String inputs, float secondaryChance, int tier) {
 		boolean isFoodRecipe = false;
 		boolean forceReturnContainer = false;
-		this.addProcessorRecipe(output, secondaryOutput, inputs, isFoodRecipe, secondaryChance, forceReturnContainer, tier);
+		
+		if(owner.testForMod("DCsAppleMilk")) 
+			new AddProcessorRecipe(owner, output, secondaryOutput, inputs, isFoodRecipe, secondaryChance, forceReturnContainer, tier);
 	}
 	
 	public void addFoodProcessorRecipe(String output, String inputs, boolean forceReturnContainer) {
@@ -44,15 +48,24 @@ public class AppleMilkTea2 extends OwnedObject {
 		float secondaryChance = 1;
 		int tier = 1;
 		
-		this.addProcessorRecipe(output, secondaryOutput, inputs, isFoodRecipe, secondaryChance, forceReturnContainer, tier);
+		if(owner.testForMod("DCsAppleMilk")) 
+			new AddProcessorRecipe(owner, output, secondaryOutput, inputs, isFoodRecipe, secondaryChance, forceReturnContainer, tier);
 	}
 	
 	public void removeIronPlateRecipe(String output) {
 		if(owner.testForMod("DCsAppleMilk")) new RemoveIronPlateRecipe(owner, output);
 	}
 	
-	public void removeProcessorRecipe(String output, boolean isFoodRecipe) {
-		if(owner.testForMod("DCsAppleMilk")) new RemoveProcessorRecipe(owner, output, isFoodRecipe);
+	public void removeJawCrusherRecipe(String output) {
+		boolean isFoodRecipe = false;
+		if(owner.testForMod("DCsAppleMilk")) 
+			new RemoveProcessorRecipe(owner, output, isFoodRecipe);
+	}
+	
+	public void removeFoodProcessorRecipe(String output) {
+		boolean isFoodRecipe = true;
+		if(owner.testForMod("DCsAppleMilk")) 
+			new RemoveProcessorRecipe(owner, output, isFoodRecipe);
 	}
 	
 	public void removeDryingRecipe(String output) {
